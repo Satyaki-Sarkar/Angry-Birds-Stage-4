17afe0e5415cd21f2700440dc2f57d1c6170c5de
Satyaki <satyakisarkar7500@gmail.com>
2020-09-04 17:51:22 +0530

    Stage 4

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -16,12 +19,25 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,250,70,38,150);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48,22,8);
+            strokeWeight(10);
+            if(pointA.x<=250){
+                line(pointA.x+25, pointA.y, pointB.x+40, pointB.y+18);
+                bird.display();
+                line(pointA.x-25, pointA.y, pointB.x-10, pointB.y+15);
+                image(this.sling3,pointA.x-25,pointA.y-5,10,30);
+            } else {
+                line(pointA.x-25, pointA.y, pointB.x+40, pointB.y+18);
+                bird.display();
+                line(pointA.x+25, pointA.y, pointB.x-10, pointB.y+15);
+                image(this.sling3,pointA.x+25,pointA.y-5,10,30);
+            }
         }
+        image(this.sling2,225,60,38,100);
     }
     
 }
